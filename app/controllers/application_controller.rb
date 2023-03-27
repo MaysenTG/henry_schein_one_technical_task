ca6e8a15f4d1c51c97cdcde6a4676769a9346c62
Maysen Greenwood <greenwood.maysen@gmail.com>
2023-03-27 21:29:07 +1300

    add redirect so clicking sign in button on question (if not logged in), the redirect_to param is set to the question path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
 class ApplicationController < ActionController::Base
   # Add custom columns to the Devise sign up form
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def after_sign_in_path_for(resource)
+    redirect_to = params[:redirect_to]
     
-    protected
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation])
+    # If the redirect_to doesn't begin with a /, add it
+    if redirect_to && redirect_to[0] != "/"
+      redirect_to = "/" + redirect_to
     end
+    
+    return redirect_to
+    
+  end
+    
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation])
+  end
 end
